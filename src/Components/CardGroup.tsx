6f324216f5c1a0bdf9f2d511e003d2290d29a03e
Chandankrswain <mr.chandanswain@gmail.com>
2024-08-31 22:54:44 +0530

    more tweaks in cardgroup

--- src/Components/CardGroup.tsx
+++ src/Components/CardGroup.tsx
@@ -1,15 +1,15 @@
 const CardGroup = () => {
   return (
     <div className="flex m-4 items-center">
-      <div className=" flex flex-col justify-between bg-red-900 w-[30%] h-[180px] rounded-2xl p-4 ">
+      <div className=" flex flex-col justify-between  w-[30%] h-[180px] rounded-2xl p-4 ">
         <p className="text-white text-8xl font-bold ">393</p>
         <p className="text-white text-xl ml-1">Total Network length</p>
       </div>
-      <div className="flex flex-col justify-between bg-red-900 w-[30%] h-[180px] rounded-2xl p-4  ">
+      <div className="flex flex-col justify-between  w-[30%] h-[180px] rounded-2xl p-4  ">
         <p className="text-white text-8xl font-bold ">12 </p>
         <p className="text-white text-xl ml-2">Total no of lines</p>
       </div>
-      <div className="flex flex-col justify-between bg-red-900 w-[30%] h-[180px] rounded-2xl p-4 ">
+      <div className="flex flex-col justify-between w-[30%] h-[180px] rounded-2xl p-4 ">
         <p className="text-white text-8xl font-bold ">288 </p>
         <p className="text-white text-xl ml-2">Total no of stations</p>
       </div>
