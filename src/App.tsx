0be2524c1669bdfa97a95d70cf6a6924466f0029
Chandankrswain <mr.chandanswain@gmail.com>
2024-09-02 23:27:51 +0530

    ADD : Services Card

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import CardGroup from "./Components/CardGroup";
 import KnowYourStation from "./Components/KnowYourStation";
 import Navbar from "./Components/Navbar";
 import NoticesAlerts from "./Components/NoticesAlerts";
+import ServicesCards from "./Components/ServicesCards";
 
 const App = () => {
   return (
@@ -10,13 +11,20 @@ const App = () => {
       <Navbar />
       <Banner />
       <NoticesAlerts />
-      <div className="flex w-full justify-center items-center mb-16">
+      <div className="flex w-full justify-center items-center mb-[10rem]">
         <div className="sm:flex sm:w-9/12">
           <KnowYourStation />
           <CardGroup />
         </div>
       </div>
-      <p className="text-white">hello</p>
+      <div className="flex w-full justify-center items-center mb-16">
+        <div className="flex sm:flex-col sm:w-9/12 items-center justify-center">
+          <p className="text-white sm:text-[96px] font-extrabold leading-none mb-16">
+            OUR SERVICE.
+          </p>
+          <ServicesCards />
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/Components/ServicesCards.tsx
@@ -0,0 +1,114 @@
+import { HiArrowUpRight } from "react-icons/hi2";
+
+const ServicesCards = () => {
+  return (
+    <div className="flex flex-wrap justify-between ">
+      <div className="flex sm:flex-col w-[307px] h-[338px] justify-between border-t border-b border-[#7a7a7a] mb-5">
+        <HiArrowUpRight className="text-white sm:w-8 sm:h-7 " />
+        <div>
+          <img />
+          <button className="text-white">NOTE</button>
+        </div>
+        <p className="text-white text-[20px] font-bold">Fare Calculator</p>
+        <p className="text-white text-[14px]">
+          The Fare Calculator is a quick and easy tool to help you estimate your
+          travel costs. Simply enter your starting and ending stations
+        </p>
+        <p className="text-[#00ffff]">FARE CAL</p>
+      </div>
+      <div className="flex sm:flex-col w-[307px] h-[338px] justify-between border-t border-b border-[#7a7a7a]">
+        <HiArrowUpRight className="text-white sm:w-8 sm:h-7 " />
+        <div>
+          <img />
+          <button className="text-white">NOTE</button>
+        </div>
+        <p className="text-white text-[20px] font-bold">Fare Calculator</p>
+        <p className="text-white text-[14px]">
+          The Fare Calculator is a quick and easy tool to help you estimate your
+          travel costs. Simply enter your starting and ending stations
+        </p>
+        <p className="text-[#00ffff]">FARE CAL</p>
+      </div>
+      <div className="flex sm:flex-col w-[307px] h-[338px] justify-between border-t border-b border-[#7a7a7a]">
+        <HiArrowUpRight className="text-white sm:w-8 sm:h-7 " />
+        <div>
+          <img />
+          <button className="text-white">NOTE</button>
+        </div>
+        <p className="text-white text-[20px] font-bold">Fare Calculator</p>
+        <p className="text-white text-[14px]">
+          The Fare Calculator is a quick and easy tool to help you estimate your
+          travel costs. Simply enter your starting and ending stations
+        </p>
+        <p className="text-[#00ffff]">FARE CAL</p>
+      </div>
+      <div className="flex sm:flex-col w-[307px] h-[338px] justify-between border-t border-b border-[#7a7a7a]">
+        <HiArrowUpRight className="text-white sm:w-8 sm:h-7 " />
+        <div>
+          <img />
+          <button className="text-white">NOTE</button>
+        </div>
+        <p className="text-white text-[20px] font-bold">Fare Calculator</p>
+        <p className="text-white text-[14px]">
+          The Fare Calculator is a quick and easy tool to help you estimate your
+          travel costs. Simply enter your starting and ending stations
+        </p>
+        <p className="text-[#00ffff]">FARE CAL</p>
+      </div>
+      <div className="flex sm:flex-col w-[307px] h-[338px] justify-between border-t border-b border-[#7a7a7a]">
+        <HiArrowUpRight className="text-white sm:w-8 sm:h-7 " />
+        <div>
+          <img />
+          <button className="text-white">NOTE</button>
+        </div>
+        <p className="text-white text-[20px] font-bold">Fare Calculator</p>
+        <p className="text-white text-[14px]">
+          The Fare Calculator is a quick and easy tool to help you estimate your
+          travel costs. Simply enter your starting and ending stations
+        </p>
+        <p className="text-[#00ffff]">FARE CAL</p>
+      </div>
+      <div className="flex sm:flex-col w-[307px] h-[338px] justify-between border-t border-b border-[#7a7a7a]">
+        <HiArrowUpRight className="text-white sm:w-8 sm:h-7 " />
+        <div>
+          <img />
+          <button className="text-white">NOTE</button>
+        </div>
+        <p className="text-white text-[20px] font-bold">Fare Calculator</p>
+        <p className="text-white text-[14px]">
+          The Fare Calculator is a quick and easy tool to help you estimate your
+          travel costs. Simply enter your starting and ending stations
+        </p>
+        <p className="text-[#00ffff]">FARE CAL</p>
+      </div>
+      <div className="flex sm:flex-col w-[307px] h-[338px] justify-between border-t border-b border-[#7a7a7a]">
+        <HiArrowUpRight className="text-white sm:w-8 sm:h-7 " />
+        <div>
+          <img />
+          <button className="text-white">NOTE</button>
+        </div>
+        <p className="text-white text-[20px] font-bold">Fare Calculator</p>
+        <p className="text-white text-[14px]">
+          The Fare Calculator is a quick and easy tool to help you estimate your
+          travel costs. Simply enter your starting and ending stations
+        </p>
+        <p className="text-[#00ffff]">FARE CAL</p>
+      </div>
+      <div className="flex sm:flex-col w-[307px] h-[338px] justify-between border-t border-b border-[#7a7a7a]">
+        <HiArrowUpRight className="text-white sm:w-8 sm:h-7 " />
+        <div>
+          <img />
+          <button className="text-white">NOTE</button>
+        </div>
+        <p className="text-white text-[20px] font-bold">Fare Calculator</p>
+        <p className="text-white text-[14px]">
+          The Fare Calculator is a quick and easy tool to help you estimate your
+          travel costs. Simply enter your starting and ending stations
+        </p>
+        <p className="text-[#00ffff]">FARE CAL</p>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesCards;
