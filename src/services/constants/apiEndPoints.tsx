4b9308c4d19fcb199ced9c57c88068303cae5f34
Chandankrswain <mr.chandanswain@gmail.com>
2024-10-11 00:25:06 +0530

    Added shortest route api

--- src/services/constants/apiEndPoints.tsx
+++ src/services/constants/apiEndPoints.tsx
@@ -1,4 +1,4 @@
 export const SEARCH_STATIONS = "/v2/en/station_by_keyword/all/:STATION_NAME";
 export const GET_STATIONS = "/v2/en/station_by_keyword/all/:STATION_NAME";
 export const ALL_LINES = "/v2/en/LINE_CODE";
-const START_END_STATION = "/v2/en/metro_line/:LINE_CODE";
+export const LEAST_DISTANCE ="/v2/en/station_route/:From_Station_Code/:To_Station_Code/least-distance/:time"
--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,5 +1,9 @@
 import apiClient from "../services/axios";
-import { ALL_LINES, SEARCH_STATIONS } from "../services/constants/apiEndPoints";
+import {
+  ALL_LINES,
+  LEAST_DISTANCE,
+  SEARCH_STATIONS,
+} from "../services/constants/apiEndPoints";
 
 export class StationData {
   client: any;
@@ -18,6 +22,19 @@ export class StationData {
     const result = await this.client.get(
       ALL_LINES.replace("LINE_CODE", keyword)
     );
+    return result?.data || [];
+  }
+
+  async leastDistance(
+    fromStationCode: string,
+    toStationCode: string,
+    time: string
+  ) {
+    const url = LEAST_DISTANCE.replace(":From_Station_Code", fromStationCode)
+      .replace(":To_Station_Code", toStationCode)
+      .replace(":time", time);
+
+    const result = await this.client.get(url);
     return result?.data || [];
   }
 }
