3b0536b80902044b29c1a1564167f5ffe5019d6a
Chandankrswain <mr.chandanswain@gmail.com>
2024-11-20 22:42:59 +0530

    some small tweaks

--- src/pages/StationInfo.tsx
+++ src/pages/StationInfo.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { useParams } from "react-router-dom";
-import { StationData } from "../utils/api";
 
 const StationInfo = () => {
   const { stationCode } = useParams();
