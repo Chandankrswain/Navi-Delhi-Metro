4696067e9a6699da164a0b0dbd9cdf8bb5d09853
Chandankrswain <mr.chandanswain@gmail.com>
2025-01-05 19:12:41 +0530

    added  metro map button

--- src/Components/BannerText.tsx
+++ src/Components/BannerText.tsx
@@ -1,6 +1,8 @@
 import { HiOutlineGlobeAlt } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
 
 const BannerText = () => {
+  const navigate = useNavigate();
   return (
     <div className="relative bg-transparent flex-col justify-center items-center justify-evenly max-w-full m-3 mb-9">
       <div>
@@ -13,19 +15,16 @@ const BannerText = () => {
         <p className="text-[3rem] sm:text-[9rem] leading-none font-bold text-white mb-3">
           Metro
         </p>
-        <a
-          className="ml:0 sm:m-2 mt-2 mb-3 text-sm sm:text-md text-white hover:text-[#04d9ff] "
-          href="../map/Network-Map-24-May-2024-Bilingual.679eea07.pdf"
-          download="Metro Map"
-        >
-          Click here to plan through Interactive map{" "}
-        </a>
       </div>
       <div className="flex bg-transparent border  sm:w-[15rem] sm:h-[4rem] text-white mt-3 sm:mt-5 sm:ml-2 p-2 font-bold w-[250px] justify-evenly justify-center items-center  ">
         <HiOutlineGlobeAlt className="w-6 h-6" />
-        <button className="font-['Share_Tech_Mono'] text-md font-thin ">
-          SELECT YOUR LANGUAGE
-        </button>
+        <a
+          target="_blank"
+          href="https://delhimetrorail.com/static/media/DMRC-Network-Map-December-2024-Bilingual.52adb9ae.pdf"
+          className="font-['Share_Tech_Mono'] text-md font-thin "
+        >
+          DOWNLOAD METRO MAP
+        </a>
       </div>
     </div>
   );
